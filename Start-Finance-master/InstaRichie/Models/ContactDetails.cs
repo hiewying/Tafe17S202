624e461429b2f1a3736bab2aadb8bf7a09d12785
Eric Foo <ericfooyx@gmail.com>
2017-09-25 14:23:40 +0930

    modified ContactDetails.cs, ContactDetailsPage.xaml, ContactDetailsPage.xaml.cs

---
+++ Start-Finance-master/InstaRichie/Models/ContactDetails.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class ContactDetails
+    {
+
+        [PrimaryKey]
+        public string contactID { get; set; }
+
+        [Unique]
+        public string firstName { get; set; }
+
+        [Unique]
+        public string lastName { get; set; }
+
+        [NotNull]
+        public string phoneNumber { get; set; }
+
+
+    }
+}
